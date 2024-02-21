a8e53ba3a7815f7976ed34759daa409291c9548e
gitsaugat <letnotsay@gmail.com>
2024-02-21 16:13:36 -0500

    Setup base CSS framework

--- analytics/urls.py
+++ analytics/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-
+from .views import Dashboard
 urlpatterns = [
+    path('',Dashboard.as_view(),name="admin_dashboard")
 ]
--- mcp/settings.py
+++ mcp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a=x2g)6yrmqs1pgc()g8d@*!ptf*0fz^5l*d57ni+%dxj=56#0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS',['*'])
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,8 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
