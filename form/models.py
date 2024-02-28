4c17a217616fcd58cfcbc776a3de4fb43f948051
gitsaugat <letnotsay@gmail.com>
2024-02-28 17:08:48 -0500

    Fix: choices field malfunctioning

--- form/models.py
+++ form/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
+import json
 # Create your models here.
 
 
@@ -15,6 +16,21 @@ class FormBuilder(models.Model):
     def __str__(self) -> str:
         return self.title
 
+    @property
+    def sections(self):
+        return Sections.objects.filter(form=self.pk).order_by('position')
+
+    @property
+    def to_json(self):
+        return {
+            'name': self.name,
+            'title': self.title,
+            'description': self.description,
+            'created_at': self.created_at,
+            'created_by': self.created_by,
+            'sections': [section.to_json for section in self.sections]
+        }
+
 
 class Sections(models.Model):
     form = models.ForeignKey(FormBuilder, on_delete=models.CASCADE)
@@ -26,25 +42,72 @@ class Sections(models.Model):
     def __str__(self) -> str:
         return self.title
 
+    @property
+    def rows(self):
+        return Row.objects.filter(section=self.pk).order_by('position')
+
+    @property
+    def to_json(self):
+
+        return {
+            'name': self.name,
+            'title': self.title,
+            'position': self.position,
+            'message': self.message,
+            'rows': [row.to_json for row in self.rows]
+        }
+
 
 class Row(models.Model):
     position = models.IntegerField()
     section = models.ForeignKey(Sections, on_delete=models.CASCADE)
     message = models.TextField()
 
+    def __str__(self) -> str:
+        return f'{self.section.title} - {str(self.position)} '
+
+    @property
+    def get_fields(self):
+        return Field.objects.filter(row=self.pk)
+
+    @property
+    def to_json(self):
+        return {
+            'position': self.position,
+            'message': self.message,
+            'fields': [field.to_json for field in self.get_fields]
+        }
+
 
 class Field(models.Model):
     choices = (('text', 'text'), ('select', 'select'), ('email', 'email'),
                ('checkbox', 'checkbox'), ('textarea', 'textarea'), ('date', 'date'),
                ('datetime', 'datetime'), ('time', 'time'))
-
+    title = models.CharField(max_length=200)
+    placeholder = models.CharField(max_length=200, null=True, blank=True)
     row = models.ForeignKey(Row, on_delete=models.CASCADE)
     input_type = models.CharField(choices=choices)
     is_multiple_choice = models.BooleanField(default=False)
+
     choices = ArrayField(
+
         ArrayField(
-            models.CharField(max_length=100, blank=True),
+
+            models.CharField(max_length=100, blank=True, null=True),
             size=20,
+            blank=True,
         ),
+        blank=True,
         size=20,
+
     )
+
+    @property
+    def to_json(self):
+        return {
+            'title': self.title,
+            'placeholder': self.placeholder,
+            'input_type': self.input_type,
+            'is_multiple_choice': self.is_multiple_choice,
+            'choices': self.choices,
+        }
--- form/views.py
+++ form/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views import View
+from django.core import serializers
+from .models import FormBuilder as FormBuilderModel
 # Create your views here.
 
 
@@ -13,20 +15,6 @@ class FormBuilder(View):
 class MentorContactRecordForm(View):
 
     def get(self, request, *args, **kwargs):
-        context = {
-            'form': {
-                'sections': [
-                    {'name': 'Mentor/Mentee Information',
-                     'fields': [{'name': 'date', 'type': 'date', 'row': 1,
-                                 'label': 'date', 'placeholder': 'Date'},
-                                {'name': 'Session Type', 'type': 'select', 'row': 1,
-                                 'label': 'session type', 'placeholder': 'session type', "choices": ['a', 'b', 'c']}]
-                     },
-                    {'name': 'What did you get your help with ? '},
-                ],
-                'name': '',
-                'id': '',
-            }
-        }
+        form = FormBuilderModel.objects.first()
 
-        return render(request=request, template_name='form/view.html', context=context)
+        return render(request=request, template_name='form/view.html', context={'form': form.to_json})
