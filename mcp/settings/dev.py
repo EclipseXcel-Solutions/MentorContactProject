efa827ec7e82a34587dc66b39b2f368b5ec413b8
gitsaugat <letnotsay@gmail.com>
2024-12-03 22:58:26 -0500

    Update:database config

--- mcp/settings/dev.py
+++ mcp/settings/dev.py
@@ -15,23 +15,37 @@ INSTALLED_APPS += [
 ]
 
 
+# DATABASES = {
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': os.environ.get('DATABASE_NAME'),
+
+#         'USER': os.environ.get('DATABASE_USER'),
+
+#         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+
+#         'HOST': os.environ.get('DATABASE_HOST'),
+
+#         'PORT': os.environ.get('DATABASE_PORT'),
+
+#     }
+# }
+
 DATABASES = {
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': os.environ.get('DATABASE_NAME'),
-
-        'USER': os.environ.get('DATABASE_USER'),
-
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-
-        'HOST': os.environ.get('DATABASE_HOST'),
-
-        'PORT': os.environ.get('DATABASE_PORT'),
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # Optional settings for better performance
+        'ATOMIC_REQUESTS': True,
+        'CONN_MAX_AGE': 600,  # Connection persistence in seconds
+        'OPTIONS': {
+            'timeout': 20,  # Database timeout in seconds
+        },
     }
 }
+
 
 STATIC_URL = '/static/'
 
