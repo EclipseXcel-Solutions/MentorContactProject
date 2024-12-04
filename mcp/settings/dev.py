c8b0f700a24bad582d0f62124b0b1c8d37d020a3
gitsaugat <letnotsay@gmail.com>
2024-12-03 23:06:26 -0500

    Change:allowed hosts

--- mcp/settings/dev.py
+++ mcp/settings/dev.py
@@ -5,7 +5,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['136.183.139.117', '0.0.0.0', 'localhost', '127.0.0.1']
 
 
 INSTALLED_APPS += [
